d6051f451cc5595b6ed1072d469322820898f423
German <germanvillapa9@gmail.com>
2020-04-04 18:06:30 -0600

    Lab6 API_KEY var

--- Lab6/app.js
+++ Lab6/app.js
@@ -1,5 +1,5 @@
 const base_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10"
-const API_KEY = "API_KEY"
+var API_KEY = "AIzaSyB8eXaYUCHSV259ixBNTk4twAPF1XFpjNI"
 
 var nextPageToken;
 var prevPageToken;
